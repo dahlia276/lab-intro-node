ef45b78f4687a3875d88fdd4509cb36f3f7a4fff
Dalia Elbanhawy <daliaelbanhawy@google.com>
2021-04-12 14:35:21 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+    }
+  
 
-  get(pos) {}
+  get(pos) {
+    if(this.length ===0 || pos>this.length-1){
+      throw new Error('OutOfBounds');
+    }else
+    return this.items[pos]
+  }
+  
 
-  max() {}
+  max() {
+    if(this.length ===0){
+      throw new Error('EmptySortedList');
+    }
+    let maximumItem = Math.max(...this.items);
+    return maximumItem
 
-  min() {}
+  }
 
-  sum() {}
+  min() {
+    if(this.length ===0){
+      throw new Error('EmptySortedList');
+    }
+    let minimumItem = Math.min(...this.items);
+    return minimumItem
 
-  avg() {}
+  }
+  
+  sum() {
+    if(this.length ===0){
+      return 0;
+    }
+    return this.items.reduce((a, b) => a + b);
+  }
+
+
+  avg() {
+    if(this.length ===0){
+      throw new Error('EmptySortedList');
+    }
+    return this.sum()/this.length;
+  }
 }
 
 module.exports = SortedList;
